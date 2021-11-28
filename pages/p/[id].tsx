b5160809ffccf19bee42c45ad59b8f32abf53da9
Glauber Nespoli <glauber.nespoli@gmail.com>
2021-11-28 17:54:47 +0100

    fixing usage of latest next-auth

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -8,7 +8,7 @@ import prisma from "../../lib/prisma";
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
     where: {
-      id: Number(params?.id) || -1,
+      id: params?.id[0],
     },
     include: {
       author: {
